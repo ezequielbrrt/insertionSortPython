e9f5dc0364bfad497b0d8ce5b16f47ff54b57666
ezequiel <ezequielbrrt@gmailcom>
2016-02-10 12:53:06 -0600

    archivo

---
+++ insertionSort.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+#algoritmo de insertion sort
+
+def insertionSort(arreglo):
+	key = arreglo[1]
+	print arreglo
+	
+
+valores = list(raw_input("Escribe los números a ordenar: "))
+insertionSort(valores)
