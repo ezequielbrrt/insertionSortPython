f282fca97615528aa77cd20a3dc4cc6007c1c50c
ezequiel <ezequielbrrt@gmailcom>
2016-02-12 22:28:29 -0600

    Algoritmo insertion sort

--- insertionSort.py
+++ insertionSort.py
@@ -1,10 +1,30 @@
 # -*- coding: utf-8 -*-
 #algoritmo de insertion sort
 
-def insertionSort(arreglo):
-	key = arreglo[1]
-	print arreglo
-	
+from tqdm import tqdm
 
-valores = list(raw_input("Escribe los números a ordenar: "))
-insertionSort(valores)
+def insertionSort(arreglo):
+	if len(arreglo) <= 1:
+		print "El arreglo ya está ordenado"
+		return
+	for x in tqdm(range(1,len(arreglo))):
+		key = arreglo[x]
+		indice = x 
+		while indice > 0 and key < arreglo[indice - 1] :
+			arreglo[indice] = arreglo[indice-1]
+			indice = indice-1
+		arreglo[indice] = key
+	return arreglo	
+		
+
+salir = 0
+valores = []
+while salir != 1:
+	try:
+		numeros = int(raw_input("Escribe los números: "))
+		valores.append(numeros)
+   	except:
+   		print "Iniciando Algoritmo"
+   		salir = 1
+
+print insertionSort(valores)   
